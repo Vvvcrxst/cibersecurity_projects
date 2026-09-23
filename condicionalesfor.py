5334d6613585d6fbdf33e449e0cafb999f3b0054
Vvvcrxst <cristianjoser20110@gmail.com>
2026-09-23 18:40:02 -0400

    practica #3
    practica basica python projecto

---
+++ condicionalesfor.py
@@ -0,0 +1,14 @@
+puerto = [22,80,443,20]
+for p in puerto :
+    if p == 22 :
+        print("Puerto 22 :Es ssh")
+    else :
+        print("5")
+    if p == 80 :
+        print("Puerto 80 : Es la web normal : https")
+    if p == 443 :
+        print("Puerto 443 : Es la web segura https")
+    if p == 20 :
+        print("Puerto 20 : Es nose")
+
+    
