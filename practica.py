c9318cbc087d1b348041562777c82fd08e8afb0f
Vvvcrxst <cristianjoser20110@gmail.com>
2026-09-23 18:34:38 -0400

    practica
    projecto basico #2

---
+++ practica.py
@@ -0,0 +1,5 @@
+ips_detectadas = ("192.168.1.10, 192.168.1.108, 192.168.1.107")
+if "192.168.1.105" in ips_detectadas:
+ print("si esta perfecto eso")
+else :
+ print("en esa lista no hay ninguna ip,prueba en otro puerto")
