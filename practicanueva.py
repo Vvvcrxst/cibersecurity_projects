9fef671b3cbf4da628d100261e077d613ae6b489
Vvvcrxst <cristianjoser20110@gmail.com>
2026-09-23 18:33:59 -0400

    idk
    pruebas projectos basicos de python #1

---
+++ practicanueva.py
@@ -0,0 +1,9 @@
+contraseña = ("123")
+if "123456" in contraseña:
+    print("correcto,adelante")
+elif "1234" in contraseña :
+    print("casi rey falto 1 caracter")
+elif "123" in contraseña :
+    print("casi")
+else :
+    print("no pa,nada parecido")
